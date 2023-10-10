c9a4bc08c9025eb52789a22d704b85d76f4b9fb4
hyeonseo0412 <103114398+diejdkll@users.noreply.github.com>
2023-10-10 16:06:45 +0900

    Revert "🔀 (#1)publishing sign in revert"

---
+++ presentation/src/main/java/com/example/miso/ui/theme/MisoTypography.kt
@@ -0,0 +1,45 @@
+package com.example.miso.ui.theme
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.miso.R
+
+object MisoTypography {
+
+    internal val inter = FontFamily(
+        Font(R.font.inter_black, FontWeight.Black),
+        Font(R.font.inter_bold, FontWeight.Bold),
+        Font(R.font.inter_extrabold, FontWeight.ExtraBold),
+        Font(R.font.inter_extralight, FontWeight.ExtraLight),
+        Font(R.font.inter_light, FontWeight.Light),
+        Font(R.font.inter_medium, FontWeight.Medium),
+        Font(R.font.inter_regular, FontWeight.Normal),
+        Font(R.font.inter_semibold, FontWeight.SemiBold),
+        Font(R.font.inter_thin, FontWeight.Thin),
+    )
+
+    @Stable
+    val title = TextStyle(
+        fontFamily = inter,
+        fontSize = 40.sp,
+        lineHeight = 16.sp
+    )
+
+    @Stable
+    val content1 = TextStyle(
+        fontFamily = inter,
+        fontSize = 15.sp,
+        lineHeight = 16.sp
+    )
+
+    @Stable
+    val content2 = TextStyle(
+        fontFamily = inter,
+        fontSize = 15.sp,
+        lineHeight = 15.sp
+    )
+}
