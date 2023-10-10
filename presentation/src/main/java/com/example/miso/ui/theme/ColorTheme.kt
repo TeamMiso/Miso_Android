9ffb414a071b1535b3b50cfd5104c53262bddfd6
송현서 <s22026@gsm.hs.kr>
2023-10-10 12:20:07 +0900

    :lipstick: Add Theme

---
+++ presentation/src/main/java/com/example/miso/ui/theme/ColorTheme.kt
@@ -0,0 +1,14 @@
+package com.example.miso.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+abstract class ColorTheme {
+    abstract val M1: Color
+
+    abstract val GRAY1: Color
+    abstract val GRAY2: Color
+
+    abstract val ERROR: Color
+    abstract val BLACK: Color
+    abstract val WHITE: Color
+}
