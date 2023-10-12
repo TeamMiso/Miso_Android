234b0f159ab6fa1533c50144d0a0344e64abd9fb
송현서 <s22026@gsm.hs.kr>
2023-10-12 13:34:32 +0900

    :bug: build bug fix

--- presentation/src/main/java/com/example/miso/ui/sign_in/SignInActivity.kt
+++ presentation/src/main/java/com/example/miso/ui/sign_in/SignInActivity.kt
@@ -3,7 +3,9 @@ package com.example.miso.ui.sign_in
 import androidx.activity.compose.setContent
 import com.example.miso.ui.base.BaseActivity
 import com.example.miso.ui.sign_in.screen.SignInScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignInActivity : BaseActivity() {
     override fun init() {
         setContent {
