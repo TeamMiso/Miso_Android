d26bb3124d59d334582337360956ea890f8564a5
송현서 <s22026@gsm.hs.kr>
2023-11-03 08:41:48 +0900

    :recycle: Datasource 추가

--- data/src/main/java/com/example/data/remote/datasource/auth/AuthDatasource.kt
+++ data/src/main/java/com/example/data/remote/datasource/auth/AuthDatasource.kt
@@ -1,9 +1,13 @@
 package com.example.data.remote.datasource.auth
 
+import com.example.data.remote.dto.auth.request.AuthLogInRequest
 import com.example.data.remote.dto.auth.request.AuthSignUpRequest
+import com.example.data.remote.dto.auth.response.AuthLogInResponse
 import kotlinx.coroutines.flow.Flow
 
 interface AuthDatasource {
     suspend fun authSignUp(body: AuthSignUpRequest): Flow<Unit>
+
+    suspend fun authLogIn(body: AuthLogInRequest): Flow<AuthLogInResponse>
 
 }
--- data/src/main/java/com/example/data/remote/datasource/auth/AuthDatasourceImpl.kt
+++ data/src/main/java/com/example/data/remote/datasource/auth/AuthDatasourceImpl.kt
@@ -2,7 +2,9 @@ package com.example.data.remote.datasource.auth
 
 import android.util.Log
 import com.example.data.remote.api.AuthAPI
+import com.example.data.remote.dto.auth.request.AuthLogInRequest
 import com.example.data.remote.dto.auth.request.AuthSignUpRequest
+import com.example.data.remote.dto.auth.response.AuthLogInResponse
 import com.example.data.util.MisoApiHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -17,6 +19,14 @@ class AuthDatasourceImpl @Inject constructor(
         emit(
             MisoApiHandler<Unit>()
                 .httpRequest { api.authSignUp(body = body) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun authLogIn(body: AuthLogInRequest): Flow<AuthLogInResponse> = flow {
+        emit(
+            MisoApiHandler<AuthLogInResponse>()
+                .httpRequest { api.authLogIn(body = body) }
                 .sendRequest()
         )
     }.flowOn(Dispatchers.IO)
