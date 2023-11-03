58ca5a1853ce81ba579bcbc5734c00d21b5443da
송현서 <s22026@gsm.hs.kr>
2023-11-03 11:15:20 +0900

    :sparkles: Repository 추가

--- domain/src/main/java/com/example/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthRepository.kt
@@ -1,8 +1,12 @@
 package com.example.domain.repository
 
+import com.example.domain.model.auth.request.AuthLogInRequestModel
 import com.example.domain.model.auth.request.AuthSignUpRequestModel
+import com.example.domain.model.auth.response.AuthLogInResponseModel
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
     suspend fun authSignUp(body: AuthSignUpRequestModel): Flow<Unit>
+
+    suspend fun authLogIn(body: AuthLogInRequestModel): Flow<AuthLogInResponseModel>
 }
