6927c509485918b80bdd21bfe001d7ea684f7012
송현서 <s22026@gsm.hs.kr>
2023-11-04 13:20:51 +0900

    :truck: rename RemoteDatasourceModule

--- di/src/main/java/com/example/di/module/RemoteDatasourceModule.kt
+++ di/src/main/java/com/example/di/module/RemoteDatasourceModule.kt
@@ -11,7 +11,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class DatasourceModule {
+abstract class RemoteDatasourceModule {
 
     @Binds
     abstract fun provideAuthDatasource(
