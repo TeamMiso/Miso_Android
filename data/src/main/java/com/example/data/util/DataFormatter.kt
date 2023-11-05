a808ed877725f1def73d77ec2dd98e2f44d89eb7
송현서 <s22026@gsm.hs.kr>
2023-11-05 23:16:38 +0900

    :bug: fix authinterceptor

--- data/src/main/java/com/example/data/util/DataFormatter.kt
+++ data/src/main/java/com/example/data/util/DataFormatter.kt
@@ -1,21 +1,21 @@
 package com.example.data.util
 
 import android.annotation.SuppressLint
-import com.example.domain.exception.TokenExpiration
+import com.example.domain.exception.TokenExpirationException
 import java.text.SimpleDateFormat
 import java.util.Date
 
 @SuppressLint("SimpleDateFormat")
 fun String.toDate(): Date {
     kotlin.runCatching {
-        SimpleDateFormat("yyyy-MM-d'T'HH:mm:ss").parse(this)!!
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(this)!!
     }.onSuccess {
         return it
     }
-    throw TokenExpiration()
+    throw TokenExpirationException()
 }
 
 @SuppressLint("SimpleDateFormat")
 fun Long.toMisoTimeDate(): Date {
-    return SimpleDateFormat("yyyy-MM-d'T'HH:mm:ss").format(this).toDate()
+    return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(this).toDate()
 }
--- domain/src/main/java/com/example/domain/exception/TokenExpirationException.kt
+++ domain/src/main/java/com/example/domain/exception/TokenExpirationException.kt
@@ -2,7 +2,7 @@ package com.example.domain.exception
 
 import java.io.IOException
 
-class TokenExpiration : IOException() {
+class TokenExpirationException : IOException() {
     override val message: String
         get() = "토큰이 만료되었습니다. 다시 로그인 해주세요"
 }
