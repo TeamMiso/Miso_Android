fc933c40d5e57d104af594d0006baa4514df877a
송현서 <s22026@gsm.hs.kr>
2023-11-06 14:37:42 +0900

    :sparkles: create user datasource

---
+++ data/src/main/java/com/example/data/remote/datasource/user/UserDatasource.kt
@@ -0,0 +1,8 @@
+package com.example.data.remote.datasource.user
+
+import com.example.data.remote.dto.user.response.UserInfoResponse
+import kotlinx.coroutines.flow.Flow
+
+interface UserDatasource {
+    suspend fun getUserInfo(): Flow<UserInfoResponse>
+}
