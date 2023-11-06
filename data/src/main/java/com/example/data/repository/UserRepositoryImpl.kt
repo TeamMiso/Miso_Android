532b975b5882d7154cdbfc9a9dfc6dab5abd770c
송현서 <s22026@gsm.hs.kr>
2023-11-06 14:39:36 +0900

    :sparkles: create user repository

---
+++ data/src/main/java/com/example/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.example.data.repository
+
+import com.example.data.local.datasource.user.LocalUserDataSource
+import com.example.data.remote.datasource.user.UserDatasource
+import com.example.data.remote.dto.user.response.toUserModel
+import com.example.domain.model.user.response.UserInfoResponseModel
+import com.example.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val localUserDataSource: LocalUserDataSource,
+    private val remoteUserDatasource: UserDatasource
+): UserRepository {
+    override suspend fun getUserInfo(): Flow<UserInfoResponseModel> {
+        return remoteUserDatasource.getUserInfo().map { it.toUserModel() }
+    }
+
+    override suspend fun saveUserInfo(userInfo: UserInfoResponseModel) {
+        userInfo.let {
+            localUserDataSource.setEmail(it.email)
+            localUserDataSource.setPassword(it.password)
+            localUserDataSource.setPoint(it.point)
+            localUserDataSource.setRole(it.role)
+        }
+    }
+
+    override suspend fun getRole(): Flow<String> {
+        return localUserDataSource.getRole()
+    }
+
+    override suspend fun deleteUserInfo() {
+        localUserDataSource.removeEmail()
+        localUserDataSource.removePassword()
+        localUserDataSource.removePoint()
+        localUserDataSource.removeRole()
+    }
+}
---
+++ domain/src/main/java/com/example/domain/repository/UserRepository.kt
@@ -0,0 +1,14 @@
+package com.example.domain.repository
+
+import com.example.domain.model.user.response.UserInfoResponseModel
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+    suspend fun getUserInfo(): Flow<UserInfoResponseModel>
+
+    suspend fun saveUserInfo(userInfo: UserInfoResponseModel)
+
+    suspend fun getRole(): Flow<String>
+
+    suspend fun deleteUserInfo()
+}
