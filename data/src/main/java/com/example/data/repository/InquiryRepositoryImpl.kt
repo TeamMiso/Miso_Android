6566e0d665333b661ce63ad5d6b253f7e332b8de
송현서 <s22026@gsm.hs.kr>
2023-11-08 19:49:50 +0900

    :sparkles: Add inquiry repository

--- data/src/main/java/com/example/data/repository/InquiryRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/InquiryRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.example.data.repository
 
 import com.example.data.remote.datasource.inquiry.InquiryDatasource
+import com.example.data.remote.dto.inquiry.response.toInquiryModel
+import com.example.domain.model.inquiry.response.InquiryListResponseModel
 import com.example.domain.repository.InquiryRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import javax.inject.Inject
@@ -19,4 +22,11 @@ class InquiryRepositoryImpl @Inject constructor(
             inquiryPart = inquiryPart
         )
     }
+
+    override suspend fun getInquiryList(): Flow<InquiryListResponseModel> {
+        return inquiryDatasource.getInquiryList().map { it.toInquiryModel() }
+    }
+
+    override suspend fun getInquiryListAll(): Flow<InquiryListResponseModel> {
+        return inquiryDatasource.getInquiryListAll().map { it.toInquiryModel() }    }
 }
--- domain/src/main/java/com/example/domain/repository/InquiryRepository.kt
+++ domain/src/main/java/com/example/domain/repository/InquiryRepository.kt
@@ -1,10 +1,15 @@
 package com.example.domain.repository
 
 import com.example.domain.model.inquiry.request.InquiryRequestModel
+import com.example.domain.model.inquiry.response.InquiryListResponseModel
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 
 interface InquiryRepository {
     suspend fun requestInquiry(filePart: MultipartBody.Part?, inquiryPart: RequestBody): Flow<Unit>
+
+    suspend fun getInquiryList(): Flow<InquiryListResponseModel>
+
+    suspend fun getInquiryListAll(): Flow<InquiryListResponseModel>
 }
