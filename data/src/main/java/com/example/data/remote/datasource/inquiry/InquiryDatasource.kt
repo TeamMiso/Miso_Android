f5e6e7da561bb3c0fde80f519e83046ade57228c
송현서 <s22026@gsm.hs.kr>
2023-11-08 19:49:22 +0900

    :sparkles: Add inquiry datasource

--- data/src/main/java/com/example/data/remote/datasource/inquiry/InquiryDatasource.kt
+++ data/src/main/java/com/example/data/remote/datasource/inquiry/InquiryDatasource.kt
@@ -1,10 +1,16 @@
 package com.example.data.remote.datasource.inquiry
 
 import com.example.data.remote.dto.inquiry.request.InquiryRequest
+import com.example.data.remote.dto.inquiry.response.InquiryListResponse
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 
 interface InquiryDatasource {
     suspend fun requestInquiry(filePart: MultipartBody.Part?, inquiryPart: RequestBody): Flow<Unit>
+
+    suspend fun getInquiryList(): Flow<InquiryListResponse>
+
+    suspend fun getInquiryListAll(): Flow<InquiryListResponse>
+
 }
