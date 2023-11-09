2118778e654fca7e4240288e584cfbc60f178836
송현서 <s22026@gsm.hs.kr>
2023-11-09 13:16:56 +0900

    :sparkles: Create adopt and unadopt api

--- data/src/main/java/com/example/data/remote/api/InquiryAPI.kt
+++ data/src/main/java/com/example/data/remote/api/InquiryAPI.kt
@@ -6,6 +6,7 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.http.GET
 import retrofit2.http.Multipart
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
@@ -29,4 +30,14 @@ interface InquiryAPI {
     suspend fun getInquiryListDetail(
         @Path("id") id: Long
     ): InquiryListDetailResponse
+
+    @PATCH("inquiry/adopt/{id}")
+    suspend fun adopt(
+        @Path("id") id: Long
+    )
+
+    @PATCH("inquiry/unadopt/{id}")
+    suspend fun unadopt(
+        @Path("id") id: Long
+    )
 }
