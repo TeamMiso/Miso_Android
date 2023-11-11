25ce5465f2935e5130943e3286bdca4bfef362fb
송현서 <s22026@gsm.hs.kr>
2023-11-11 20:22:39 +0900

    :sparkles: Add recyclables repository

--- domain/src/main/java/com/example/domain/repository/RecyclablesRepository.kt
+++ domain/src/main/java/com/example/domain/repository/RecyclablesRepository.kt
@@ -5,4 +5,12 @@ import kotlinx.coroutines.flow.Flow
 
 interface RecyclablesRepository {
     suspend fun search(search: String): Flow<SearchResponseModel>
+
+    suspend fun saveSearchHistory(searchHistory: String)
+
+    suspend fun getSearchHistory(): Flow<List<String>>
+
+    suspend fun deleteSearchHistory(index: Int)
+
+    suspend fun deleteAllSearchHistory()
 }
