5ad6a018d56ca9b9da2b884cfa3aa9b710e1ecae
GSM-Minseok <ms0818000@gmail.com>
2023-11-12 11:00:23 +0900

    :sparkles: add CameraDatasource

---
+++ data/src/main/java/com/example/data/remote/datasource/camera/CameraDatasourceImpl.kt
@@ -0,0 +1,24 @@
+package com.example.data.remote.datasource.camera
+
+import com.example.data.remote.api.CameraAPI
+import com.example.data.remote.dto.camera.request.CameraRequest
+import com.example.data.remote.dto.camera.response.CameraResponse
+import com.example.data.util.MisoApiHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class CameraDatasourceImpl @Inject constructor(
+    private val api: CameraAPI
+): CameraDatasource {
+    override suspend fun getAiResponse(body: CameraRequest): Flow<CameraResponse> = flow {
+        emit(
+            MisoApiHandler<CameraResponse>()
+                .httpRequest { api.getAiResponse(body = body) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+}
