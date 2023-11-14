85af0b96d63b1cc82741e085e8afb057db21edb2
GSM-Minseok <ms0818000@gmail.com>
2023-11-14 19:55:04 +0900

    :sparkles: add BitmapState

---
+++ presentation/src/main/java/com/example/miso/ui/camera/state/BitmapState.kt
@@ -0,0 +1,6 @@
+package com.example.miso.ui.camera.state
+
+data class BitmapState(
+    val uploadedBitmap: Boolean? = null,
+    val callSendBitmap: Boolean? = null
+)
--- presentation/src/main/java/com/example/miso/ui/camera/state/UploadedBitmapState.kt
+++
@@ -1,5 +0,0 @@
-package com.example.miso.ui.camera.state
-
-data class UploadedBitmapState(
-    val uploadedBitmap: Boolean? = null
-)
