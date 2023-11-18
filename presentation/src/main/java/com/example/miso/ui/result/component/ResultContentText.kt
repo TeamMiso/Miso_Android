2b2e68d5c28be5556f5630460b4731d67a3abe97
송현서 <s22026@gsm.hs.kr>
2023-11-18 14:26:59 +0900

    :lipstick: Change text to markdowntext

--- presentation/src/main/java/com/example/miso/ui/result/component/ResultContentText.kt
+++ presentation/src/main/java/com/example/miso/ui/result/component/ResultContentText.kt
@@ -1,18 +1,16 @@
 package com.example.miso.ui.result.component
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontWeight
 import com.example.miso.ui.theme.MisoTheme
+import dev.jeziellago.compose.markdowntext.MarkdownText
 
 @Composable
-fun ResultContentText(text: String) {
+fun ResultContentText(markdown: String) {
     MisoTheme { colors, typography ->
-        Text(
-            text = text,
-            color = colors.BLACK,
+        MarkdownText(
+            markdown = markdown,
             style = typography.content4,
-            fontWeight = FontWeight.ExtraLight
+            color = colors.BLACK
         )
     }
 }
