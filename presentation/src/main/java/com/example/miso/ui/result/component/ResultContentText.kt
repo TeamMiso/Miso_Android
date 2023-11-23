1c2fbd6c07fa1facb4da9f09a86298bf722cd166
송현서 <s22026@gsm.hs.kr>
2023-11-23 10:34:51 +0900

    :lipstick: Change markdowntext style

--- presentation/src/main/java/com/example/miso/ui/result/component/ResultContentText.kt
+++ presentation/src/main/java/com/example/miso/ui/result/component/ResultContentText.kt
@@ -1,15 +1,18 @@
 package com.example.miso.ui.result.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+import com.example.miso.R
 import com.example.miso.ui.theme.MisoTheme
 import dev.jeziellago.compose.markdowntext.MarkdownText
 
 @Composable
 fun ResultContentText(markdown: String) {
-    MisoTheme { colors, typography ->
+    MisoTheme { colors, _ ->
         MarkdownText(
             markdown = markdown,
-            style = typography.content4,
+            fontResource = R.font.suitv1_extralight,
+            fontSize = 10.sp,
             color = colors.BLACK
         )
     }
