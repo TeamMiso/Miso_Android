d0bcf45802d9cc9808669b2d1e73141908d26d96
GSM-Minseok <ms0818000@gmail.com>
2023-11-24 01:07:42 +0900

    :sparkles: add PurchaseRepository

---
+++ domain/src/main/java/com/example/domain/repository/PurchaseRepository.kt
@@ -0,0 +1,9 @@
+package com.example.domain.repository
+
+import com.example.domain.model.purchase.response.PurchaseListResponseModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface PurchaseRepository {
+    suspend fun getPurchase(): Flow<PurchaseListResponseModel>
+}
