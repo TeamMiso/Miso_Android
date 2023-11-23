10094e98d5b1f5da58e411635c53ba86f3aa14e6
GSM-Minseok <ms0818000@gmail.com>
2023-11-24 01:23:20 +0900

    :sparkles: add buyItem

--- data/src/main/java/com/example/data/remote/api/PurchaseAPI.kt
+++ data/src/main/java/com/example/data/remote/api/PurchaseAPI.kt
@@ -2,8 +2,15 @@ package com.example.data.remote.api
 
 import com.example.data.remote.dto.purchase.response.PurchaseListResponse
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface PurchaseAPI {
-    @GET
+    @GET("purchase")
     suspend fun getPurchaseList(): PurchaseListResponse
+
+    @POST("purchase/{id}")
+    suspend fun buyItem(
+        @Path("id") id: Long
+    )
 }
