306257987fb1126d818eec5c991967ee0a8b129b
GSM-Minseok <ms0818000@gmail.com>
2023-11-25 14:36:13 +0900

    :sparkles: add purchaseList component

---
+++ presentation/src/main/java/com/example/miso/ui/purchase/component/PurchaseListItem.kt
@@ -0,0 +1,77 @@
+package com.example.miso.ui.purchase.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.miso.ui.theme.MisoTheme
+
+@Composable
+fun InquiryListItem(
+    date: String,
+    title: String,
+    price: String,
+) {
+    MisoTheme { colors, typography ->
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(40.dp)
+        ) {
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(
+                            start = 12.dp,
+                            end = 18.dp
+                        ),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = date,
+                        color = colors.GRAY5,
+                        style = typography.content4,
+                        fontWeight = FontWeight.ExtraLight
+                    )
+                    Spacer(modifier = Modifier.width(50.dp))
+                    Text(
+                        text = title,
+                        color = colors.GRAY5,
+                        style = typography.content4,
+                        fontWeight = FontWeight.ExtraLight
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = price,
+                        color = colors.GRAY5,
+                        style = typography.content4,
+                        fontWeight = FontWeight.ExtraLight
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun InquiryListItemPreView() {
+    InquiryListItem("23.01.23", "츄파츕스", "100")
+}
---
+++ presentation/src/main/java/com/example/miso/ui/purchase/component/PurchaseSticyHeader.kt
@@ -0,0 +1,66 @@
+package com.example.miso.ui.purchase.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.miso.ui.theme.MisoTheme
+@Composable
+fun PurchaseStickyHeader() {
+    MisoTheme { colors, typography ->
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(40.dp)
+            .background(colors.GRAY3)
+        ) {
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 14.dp, end = 18.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "구매일",
+                        color = colors.GRAY6,
+                        style = typography.content3,
+                        fontWeight = FontWeight.ExtraLight
+                    )
+                    Spacer(modifier = Modifier.width(55.dp))
+                    Text(
+                        text = "상품",
+                        color = colors.GRAY6,
+                        style = typography.content3,
+                        fontWeight = FontWeight.ExtraLight
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = "가격",
+                        color = colors.GRAY6,
+                        style = typography.content3,
+                        fontWeight = FontWeight.ExtraLight
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PurchaseStickyHeaderPreview() {
+    PurchaseStickyHeader()
+}
