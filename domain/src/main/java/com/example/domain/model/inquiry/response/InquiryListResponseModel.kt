36e2c9a1bdbf4094d1b28533b6f5f1f32aac3372
송현서 <s22026@gsm.hs.kr>
2023-12-06 09:01:23 +0900

    :sparkles: :: Add GetAllRecyclablesList usecase

--- domain/src/main/java/com/example/domain/model/inquiry/response/InquiryListResponseModel.kt
+++ domain/src/main/java/com/example/domain/model/inquiry/response/InquiryListResponseModel.kt
@@ -3,6 +3,7 @@ package com.example.domain.model.inquiry.response
 data class InquiryListResponseModel(
     val inquiryList: List<InquiryListModel>
 )
+
 data class InquiryListModel(
     val id: Long,
     val inquiryDate: String,
