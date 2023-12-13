1b7b126a4c21355fcc28d459cbf423f3e0f5a6ef
GSM-Minseok <ms0818000@gmail.com>
2023-12-13 19:47:20 +0900

    :memo: Add SelectPhotoPathBottomSheet.kt

--- presentation/src/main/java/com/example/miso/ui/inquiry/component/bottomsheet/SelectPhotoPathBottomSheet.kt
+++ presentation/src/main/java/com/example/miso/ui/inquiry/component/bottomsheet/SelectPhotoPathBottomSheet.kt
@@ -1,5 +1,9 @@
 package com.example.miso.ui.inquiry.component.bottomsheet
 
+import android.Manifest
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +12,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,3 +28,42 @@ import com.example.miso.ui.component.icon.GalleryIcon
 import com.example.miso.ui.inquiry.component.InquiryButton
 import com.example.miso.ui.theme.MisoTheme
 
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SelectPhotoPathBottomSheet(
+    bottomSheetState: ModalBottomSheetState,
+    onProfileImageUriChanged: () -> Unit
+) {
+    val isCamera = remember { mutableStateOf(false) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        when {
+            isGranted && isCamera.value -> {
+
+            }
+            isGranted && !isCamera.value -> {
+
+            }
+        }
+    }
+    val permission =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_IMAGES
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+
+    SelectPhotoPathBottomSheetComponent(
+        bottomSheetState = bottomSheetState,
+        onGalleryLaunchButtonClick = {
+            isCamera.value = false
+            permissionLauncher.launch(permission)
+        },
+        onCameraLaunchButtonClick = {
+            isCamera.value = true
+            permissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    )
+}
